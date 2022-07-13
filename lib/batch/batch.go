89366e62275c7146ca9a3875da5b7363fc8dd557
GTsirekidze <91791311+GTsirekidze@users.noreply.github.com>
2022-07-13 22:04:00 +0400

    Update batch.go

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,7 +1,6 @@
 package batch
 
 import (
-	"fmt"
 	"sync"
 	"time"
 )
@@ -20,44 +19,36 @@ func getOne(id int64) user {
 func getBatch(n int64, pool int64) (res []user) {
 	c := make(chan user)
 	var i int64 = 0
-	var wg sync.WaitGroup
+	var wg, wg2 sync.WaitGroup
 	wg.Add(int(pool))
-
-	go consume(c, &res)
+	wg2.Add(1)
+	go consume(c, &res, &wg2)
 
 	for ; i < pool; i++ {
-
 		go getNUser(n, pool, i, c, &wg)
 	}
 
 	wg.Wait()
-
 	close(c)
-
-	fmt.Println("HERE ends everything", res)
+	wg2.Wait()
 	return res
 }
 
-func consume(ch chan user, res *[]user) {
+func consume(ch chan user, res *[]user, wg *sync.WaitGroup) {
 
-	//fmt.Println("HERE ends", res, <-ch)
-	//fmt.Println("index is")
 	for i := range ch {
 		Mu.Lock()
 		*res = append(*res, i)
 		Mu.Unlock()
-		//fmt.Println("HERE ends", res, i)
 	}
+	defer wg.Done()
 
 }
 
 func getNUser(n int64, pool int64, routineIndex int64, userChan chan user, wg *sync.WaitGroup) {
-	//fmt.Println("123456789")
 	k := routineIndex
 	for ; k < n; k += pool {
-
 		userChan <- getOne(k)
-
 	}
 	wg.Done()
 }
