a33a82905ff80b22f91a4beff3c2f5c34ef9b667
GTsirekidze <g_tsirekidze@cu.edu.ge>
2022-07-13 11:19:33 +0400

    ""

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -17,19 +17,18 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	c := make(chan user, n)
+	c := make(chan user)
 	var i int64 = 0
 	var wg sync.WaitGroup
+	go consume(c, &res)
 	for ; i < pool; i++ {
 		wg.Add(1)
 		go getNUser(n, pool, i, c, &wg)
 	}
 	//fmt.Println("after wait")
-	wg.Wait()
 
 	//fmt.Println("after close")
-	wg.Add(1)
-	go consume(c, &res, &wg)
+
 	wg.Wait()
 
 	close(c)
@@ -37,32 +36,30 @@ func getBatch(n int64, pool int64) (res []user) {
 	return res
 }
 
-func consume(ch chan user, res *[]user, wg *sync.WaitGroup) {
-
+func consume(ch chan user, res *[]user) {
 	for i := range ch {
 		fmt.Println("HERE o", i)
 		*res = append(*res, i)
 		fmt.Println("HERE or not", res)
 		//fmt.Println("HERE or not end", res)
 	}
-	wg.Done()
 }
 
 func getNUser(n int64, pool int64, routineIndex int64, userChan chan user, wg *sync.WaitGroup) {
-	//fmt.Println("123456789")
+	fmt.Println("123456789")
 	var wtg sync.WaitGroup
 
 	//for ; routineIndex <= n; routineIndex+=pool {
 	for ; routineIndex < n; routineIndex += pool {
 		wtg.Add(1)
 		//fmt.Println("deadlock HERE or not", routineIndex)
-		go func(k int64, wtg *sync.WaitGroup) {
-			//fmt.Println("ping routine K", k)
-			userChan <- getOne(k)
-			//fmt.Println("ping routine before  K done", k)
-			defer wtg.Done()
-			//fmt.Println("ping routine after  K done", k)
-		}(routineIndex, &wtg)
+		//go func(k int64, wtg *sync.WaitGroup) {
+		//fmt.Println("ping routine K", k)
+		userChan <- getOne(routineIndex)
+		//fmt.Println("ping routine before  K done", k)
+		wtg.Done()
+		//fmt.Println("ping routine after  K done", k)
+		//}(routineIndex, &wtg)
 		//fmt.Println("deadlock free HERE or not", routineIndex)
 	}
 	wtg.Wait()
